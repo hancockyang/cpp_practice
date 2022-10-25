7919a1047d84c613140d7198f56f0a1a43b6f8f8
hancockyang <hancock.yang@gmail.com>
2022-10-24 21:41:30 -0400

    Add State

--- Design_Pattern/Behavioral/README.md
+++ Design_Pattern/Behavioral/README.md
@@ -1,47 +1,37 @@
-# Behavioral Design pattern 
+# Behavioral Design pattern  
 
 ## Command  
 
-
 ![alt text](https://github.com/hancockyang/cpp_practice/blob/main/Design_Pattern/Behavioral/Command-Game.PNG)
-
 
 ## Chain of Responsibility
 
-
 ![alt text](https://github.com/hancockyang/cpp_practice/blob/main/Design_Pattern/Behavioral/Chain-of-responsibility-photoshop.PNG)
-
 
 ## Mediator
 
-
 ![alt text](https://github.com/hancockyang/cpp_practice/blob/main/Design_Pattern/Behavioral/Mediator.PNG)
-
 
 ## Memento
 
-
 ![alt text](https://github.com/hancockyang/cpp_practice/blob/main/Design_Pattern/Behavioral/Memento.PNG)
 
-
-## Strategy 
-
+## Strategy  
 
 ![alt text](https://github.com/hancockyang/cpp_practice/blob/main/Design_Pattern/Behavioral/Strategy.PNG)
 
-## Template 
-
+## Template  
 
 ![alt text](https://github.com/hancockyang/cpp_practice/blob/main/Design_Pattern/Behavioral/Template.PNG)
 
-
-## Visitor 
-
+## Visitor  
 
 ![alt text](https://github.com/hancockyang/cpp_practice/blob/main/Design_Pattern/Behavioral/Visitor.PNG)
 
-
-## Observer 
-
+## Observer  
 
 ![alt text](https://github.com/hancockyang/cpp_practice/blob/main/Design_Pattern/Behavioral/Observer.PNG)
+
+## State  
+
+![alt text](https://github.com/hancockyang/cpp_practice/blob/main/Design_Pattern/Behavioral/State.PNG):w
---
+++ Design_Pattern/Behavioral/State.cpp
@@ -0,0 +1,141 @@
+#include<iostream>
+#include<string>
+#include<memory>
+
+class Context;
+
+class IState
+{
+    public:
+        virtual ~IState() = default;
+        virtual void NextState(Context * context) const = 0;
+        virtual const std::string GetName() const = 0;
+};
+
+class Context
+{
+    public:
+
+        Context (std::unique_ptr<IState>&& state) : m_state(std::move(state)) {};
+        
+
+        void GetState() const 
+        {
+           std::cout<< m_state -> GetName();
+        }
+        void SetState(std::unique_ptr<IState> && state)
+        {
+            m_state = std::move(state);
+        }
+        void Request() 
+        {
+            m_state -> NextState(this);
+        }
+
+    private:
+        std::unique_ptr<IState>  m_state;
+};
+
+class StateA;
+class StateB;
+class StateC;
+class StateD;
+
+class StateA : public IState
+{
+    public:
+        StateA() = default;
+
+        void NextState(Context * context) const override;
+
+        const std::string GetName() const override
+        {
+            return "Current State is A\n";
+        }
+
+};
+
+class StateB : public IState
+{
+    public:
+        StateB() = default;
+
+        void NextState(Context * context) const override;
+        const std::string GetName() const override
+        {
+            return "Current State is B\n";
+        }
+
+};
+
+class StateC : public IState
+{
+    public:
+        StateC() = default;
+
+        void NextState(Context * context) const override;
+        const std::string GetName() const override
+        {
+            return "Current State is C\n";
+        }
+
+};
+
+class StateD : public IState
+{
+    public:
+        StateD() = default;
+
+        void NextState(Context * context) const override;
+        const std::string GetName() const override
+        {
+            return "Current State is D\n";
+        }
+
+};
+
+void StateD :: NextState(Context * context) const
+{
+    std::cout<<"Do some business logic for state - D"<<std::endl;
+    std::cout<<"Setting to next state..."<<std::endl;
+    context -> SetState(std::make_unique<StateA>());
+}
+
+void StateA :: NextState(Context * context) const
+{
+    std::cout<<"Do some business logic for state - A"<<std::endl;
+    std::cout<<"Setting to next state..."<<std::endl;
+    context -> SetState(std::make_unique<StateB>());
+}
+
+void StateB :: NextState(Context * context) const
+{
+    std::cout<<"Do some business logic for state - B"<<std::endl;
+    std::cout<<"Setting to next state..."<<std::endl;
+    context -> SetState(std::make_unique<StateC>());
+}
+
+void StateC :: NextState(Context * context) const
+{
+    std::cout<<"Do some business logic for state - C"<<std::endl;
+    std::cout<<"Setting to next state..."<<std::endl;
+    context -> SetState(std::make_unique<StateD>());
+};
+
+
+
+int main ()
+{
+    Context * c = new Context(std::make_unique<StateA>());
+    c -> Request();
+    c -> GetState();
+    c -> Request();
+    c -> Request();
+    c -> GetState();
+    c -> Request();
+    c -> Request();
+    c -> GetState();
+
+    delete c;
+    return 0;
+};
